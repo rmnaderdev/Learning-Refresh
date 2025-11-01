da9fdff0b4f2f5fb564516d02b82e8b68547b440
Richard Nader, Jr. <rmnaderdev@gmail.com>
2025-11-01 17:01:44 -0400

    Made one solution file. Kth Largest Element In Stream.

---
+++ KthLargestElementInStream/Program.cs
@@ -0,0 +1,39 @@
+﻿
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+Console.WriteLine(kthLargest.Add(3));   // return 4
+Console.WriteLine(kthLargest.Add(5));   // return 5
+Console.WriteLine(kthLargest.Add(10));  // return 5
+Console.WriteLine(kthLargest.Add(9));   // return 8
+Console.WriteLine(kthLargest.Add(4));   // return 8
+
+public class KthLargest
+{
+    private int _k;
+    private PriorityQueue<int, int> _minHeap;
+    
+    public KthLargest(int k, int[] nums)
+    {
+        _k = k;
+        _minHeap = new PriorityQueue<int, int>();
+
+        foreach (var num in nums)
+        {
+            _minHeap.Enqueue(num, num);
+            if (_minHeap.Count > k)
+            {
+                _minHeap.Dequeue();
+            }
+        }
+    }
+
+    public int Add(int val)
+    {
+        _minHeap.Enqueue(val, val);
+        if (_minHeap.Count > _k)
+        {
+            _minHeap.Dequeue();
+        }
+        
+        return _minHeap.Peek();
+    }
+}
