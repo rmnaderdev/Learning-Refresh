afd19d01f7902bcba22b1d3471b78aa306bb6bc2
Richard Nader, Jr. <rmnaderdev@gmail.com>
2025-11-01 18:42:24 -0400

    Merge Intervals. Started TopKrequentElements.

---
+++ MergeIntervals/Program.cs
@@ -0,0 +1,41 @@
+﻿var obj = new Solution();
+
+var arr1 = new int[][]
+{
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18]
+};
+
+var arr2 = obj.Merge(arr1);
+
+foreach (int[] interval in arr2) {
+    Console.WriteLine(interval[0] + " " + interval[1]);
+}
+
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        
+        List<int[]> res = new List<int[]>();
+        res.Add([intervals[0][0], intervals[0][1]]);
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            var last = res[^1];
+            var curr = intervals[i];
+
+            if (last[1] >= curr[0])
+            {
+                last[1] = Math.Max(last[1], curr[1]);
+            }
+            else
+            {
+                res.Add([curr[0], curr[1]]);
+            }
+        }
+        
+        return res.ToArray();
+    }
+}
---
+++ TopKFrequentElements/Program.cs
@@ -0,0 +1,42 @@
+﻿var input = new[]
+{
+    1, 1, 1, 2, 2, 3
+};
+
+var obj = new Solution();
+var res = obj.TopKFrequent(input, 2);
+
+Console.WriteLine(string.Join(",", res));
+
+public class Solution
+{
+    private Dictionary<int, int> mp = new();
+
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        foreach (var num in nums)
+        {
+            if (!mp.TryAdd(num, 1))
+            {
+                mp[num]++;
+            }
+        }
+
+        var heap = new PriorityQueue<int, int>();
+
+        foreach (var kv in mp)
+        {
+            heap.Enqueue(kv.Key, kv.Value);
+            if (heap.Count > k)
+            {
+                heap.Dequeue();
+            }
+        }
+        
+        List<int> res = new List<int>();
+        while (heap.Count > 0)
+        {
+            res.Insert(0, heap.);
+        }
+    }
+}
